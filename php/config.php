14faad56f547c689a2ca2a13aa878b2a9ad291a0
nguyenbenjamin3 <54734324+nguyenbenjamin3@users.noreply.github.com>
2023-10-02 20:55:11 -0700

    Update config.php to Benjamin

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?
 	//define the receiver of the email
 	
-	define('TO_NAME','Dave Winter');
-	define('TO_EMAIL','dave.winter@csulb.edu');
+	define('TO_NAME','Benjamin Nguyen');
+	define('TO_EMAIL','nguyenbenjamin3@gmail.com');
 	define('SUBJECT','message from your resume website');	
 	
 	define('TEMPLATE_PATH','template/default.php');
